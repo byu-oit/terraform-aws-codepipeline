773373343ffe3813e82ed1aacbaad801476f7b5e
garygsc <GaryGSC@users.noreply.github.com>
2019-12-20 09:43:54 -0700

    Update CodeBuild image to aws/codebuild/standard:3.0
    Closes #2.

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "aws_codebuild_project" "build-project" {
   }
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:2.0"
+    image           = "aws/codebuild/standard:3.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
     environment_variable {
